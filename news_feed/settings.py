3482357dc30eaab8f99cddb654dae9b3f1af285c
Achraf Marbati <64266975+SayC0der@users.noreply.github.com>
2020-10-23 18:42:43 +0200

    Update settings.py
    Security edition.

--- news_feed/settings.py
+++ news_feed/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 't35v=b^h$@#dq#pr0)3t$^wg)elpzczl49ym0(g920x**uqksd'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
